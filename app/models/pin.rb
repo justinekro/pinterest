24adfce88820fcdb0c2a9f639609a16eeb5be6bd
Justine <>
2018-02-01 19:00:08 +0100

    ok

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,9 +1,9 @@
 class Pin < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
-  VALID_LINK_REGEX = /\A[\w+\-.]+.[a-z]+/i
+  VALID_LINK_REGEX = /(^$)|(^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$)/ix
 	validates :url, presence: true,
 			            format: { with: VALID_LINK_REGEX }
 
-	# Regex simple sans https:// > juste sous la forme aaaaa.c
+	# Regex qui oblige un http ou https
 end
